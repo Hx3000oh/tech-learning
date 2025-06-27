53268759d5e99c32f558182735c6fd69794b36e6
Se7s <hx3000se7s@gmail.com>
2025-06-27 15:50:12 +0300

    Codeing

--- src/ts/script.ts
+++ src/ts/script.ts
@@ -2,4 +2,9 @@
 
 const landing = document.querySelector("section.landing") as HTMLDivElement;
 
-console.log(landing);
+const cont = landing.querySelector(".container") as HTMLDivElement;
+const overlay = landing.querySelector(".overlay") as HTMLDivElement;
+
+cont.style.height = `${landing.scrollHeight}px`;
+
+overlay.style.height = `${landing.scrollHeight}px`;
