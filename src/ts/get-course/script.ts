9ee883c43115b39eedbee1baff949649b231cb84
hx3000oh <hx3000se7s@gmail.com>
2025-07-12 22:56:21 +0300

    Contact Part 3

--- src/ts/get-course/script.ts
+++ src/ts/get-course/script.ts
@@ -1,3 +1,33 @@
 ///<reference path="../base.ts">
-const blocksArea = document.querySelector("section.contact .blocks-area") as HTMLDivElement;
-// console.log(blocksArea)
+const blocksArea = document.querySelector(
+  "section.contact .blocks-area",
+) as HTMLDivElement;
+const blocks = blocksArea.querySelectorAll(
+  ".block",
+) as NodeListOf<HTMLDivElement>;
+
+const menusArea = document.querySelector(".menus-area") as HTMLDivElement;
+
+blocks.forEach((block) => {
+  block.addEventListener("click", () => {
+    const overlay = document.querySelector(".overlay") as HTMLDivElement;
+    overlay.classList.toggle("active");
+    menusArea.querySelector(`#${block.id}-menu`)?.classList.toggle("active");
+    menusArea.querySelectorAll(".close")?.forEach((close) => {
+      close.addEventListener("click", () => {
+        overlay.classList.remove("active");
+        menusArea
+          .querySelector(`#${block.id}-menu`)
+          ?.classList.remove("active");
+      });
+    });
+    document.addEventListener("keydown", (e) => {
+      if (e.key === "Escape") {
+        overlay.classList.remove("active");
+        menusArea
+          .querySelector(`#${block.id}-menu`)
+          ?.classList.remove("active");
+      }
+    });
+  });
+});
