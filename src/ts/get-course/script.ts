762cca6ce8bd9ab4df6cf338faffd0bdab668a44
Se7s <hx3000se7s@gmail.com>
2025-07-13 18:14:56 +0300

    Contact Part 4

--- src/ts/get-course/script.ts
+++ src/ts/get-course/script.ts
@@ -11,8 +11,13 @@ const menusArea = document.querySelector(".menus-area") as HTMLDivElement;
 blocks.forEach((block) => {
   block.addEventListener("click", () => {
     const overlay = document.querySelector(".overlay") as HTMLDivElement;
+
+    // Opem Menu
     overlay.classList.toggle("active");
     menusArea.querySelector(`#${block.id}-menu`)?.classList.toggle("active");
+
+    // Close Menu
+    // -- With Close Button
     menusArea.querySelectorAll(".close")?.forEach((close) => {
       close.addEventListener("click", () => {
         overlay.classList.remove("active");
@@ -20,7 +25,9 @@ blocks.forEach((block) => {
           .querySelector(`#${block.id}-menu`)
           ?.classList.remove("active");
       });
+      clearEvents(close, "click");
     });
+    // -- With ESC Key
     document.addEventListener("keydown", (e) => {
       if (e.key === "Escape") {
         overlay.classList.remove("active");
@@ -28,6 +35,25 @@ blocks.forEach((block) => {
           .querySelector(`#${block.id}-menu`)
           ?.classList.remove("active");
       }
+      clearEvents(document, "keydown");
     });
+    // -- With Overlay Click
+    overlay.addEventListener("click", () => {
+      overlay.classList.remove("active");
+      menusArea.querySelector(`#${block.id}-menu`)?.classList.remove("active");
+      clearEvents(overlay, "click");
+    });
+
+    // Clear Events
+    function clearEvents(e: Element | Document, ev: string) {
+      e.removeEventListener(ev, () => {});
+    }
   });
 });
+
+function copuNumber(input: string) {
+  const item = document.getElementById(input) as HTMLInputElement;
+  if (item) {
+    item.select();
+  }
+}
